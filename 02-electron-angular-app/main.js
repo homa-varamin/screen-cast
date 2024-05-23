bf0abc9aa40b5eb0c786d4e6d7ded58a0a45058c
homa-varamin <homatechschool@gmail.com>
2024-05-23 18:14:14 +0330

    adding-angular-project

---
+++ 02-electron-angular-app/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/screen-cast-angular/dist/screen-cast-angular/index.html`);
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
