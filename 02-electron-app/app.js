9440a1aef356f8e446b03ca828d59cf4b7745d81
homa-varamin <homatechschool@gmail.com>
2024-07-26 00:56:15 +0330

    fix-file

--- 02-electron-app/app.js
+++ 02-electron-app/app.js
@@ -3,14 +3,14 @@ import express from "express";
 import path from "path";
 import cors from "cors";
 import { fileURLToPath } from "url";
-import screen from "screenshot-desktop";
-import sharp from "sharp"; // For image processing
+import fs from "fs";
+import http from 'http';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 app.use(cors());
-export const appPort = 32768;
+export const appPort = 3000; /* 32768 */
 
 const webcamOptions = {
   width: 1280,
@@ -43,7 +43,30 @@ app.get("/capture", (req, res) => {
   });
 });
 
-app.get("/stream", async (req, res) => {
+app.get('/stream', (req, res) => {
+  const filePath = path.resolve('output.mp4');
+  res.sendFile(filePath);
+});
+
+
+/* app.get('/stream', (req, res) => {
+  res.setHeader('Content-Type', 'video/x-flv');
+
+  http.get('http://localhost:8081/feed1.ffm', (streamRes) => {
+    // Check if the request was successful
+    if (streamRes.statusCode !== 200) {
+      res.sendStatus(streamRes.statusCode);
+      return;
+    }
+    // Pipe the stream response to the client
+    streamRes.pipe(res);
+  }).on('error', (err) => {
+    console.error('Error fetching stream:', err);
+    res.sendStatus(500);
+  });
+}); */
+
+/* app.get("/stream", async (req, res) => {
   try {
     const imgBuffer = await screen({
       format: "jpeg",
@@ -61,7 +84,7 @@ app.get("/stream", async (req, res) => {
     console.error("Error capturing screen:", err);
     res.status(500).send("Error capturing screen");
   }
-});
+}); */
 
 app.listen(appPort, () => {
   console.log(`App listening at http://localhost:${appPort}`);
